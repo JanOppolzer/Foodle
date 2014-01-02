ffd1665b51cc45b2e43e6ef483911d9940426494
Jan Oppolzer <jan.oppolzer@cesnet.cz>
2014-01-02 13:29:36 +0100

    Google Analytics removed

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -38,11 +38,13 @@
 
 
 
+<!--
 <script src="https://ssl.google-analytics.com/urchin.js" type="text/javascript"></script>
 <script type="text/javascript">
 _uacct = "UA-431110-13";
 urchinTracker();
 </script>
+-->
 
 </body>
 </html>
